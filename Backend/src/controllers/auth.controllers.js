af18edb14995bd04d3cbcc547f14d0f2f7240226
utkarshkumarsingh120903@gmail.com <utkarshkumarsingh120903@gmail.com>
2025-10-28 18:48:57 +0530

    Implemented Websocket

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -110,3 +110,13 @@ export const updateProfile = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 };
+
+
+export const checkAuth = async (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller: ",error.message);
+        res.status(500).json({message:"Internal server error"});
+    }
+}
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,22 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieparser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+
+//! WEB SOCKET IMPORTS
+import { app,server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
-const app = express();
 
 app.use(express.json());
 app.use(cookieparser());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:8080"],
+    credentials: true
+}));
 
 
 //! Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/messages" , messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 });
